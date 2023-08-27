62e4acf683bda010d01767dd56cb2d9a56d11dbb
vintasoft <alex@vintasoft.com>
2023-08-27 13:40:28 +0300

    Source codes update

--- CSharp/MainForm.cs
+++ CSharp/MainForm.cs
@@ -53,6 +53,9 @@ namespace TwainAdvancedDemo
 
         public MainForm()
         {
+            // register the evaluation license for VintaSoft TWAIN .NET SDK
+            Vintasoft.Twain.TwainGlobalSettings.Register("REG_USER", "REG_EMAIL", "EXPIRATION_DATE", "REG_CODE");
+
             InitializeComponent();
 
             this.Text = string.Format("VintaSoft TWAIN Advanced Demo v{0}", TwainGlobalSettings.ProductVersion);
